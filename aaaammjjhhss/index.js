0c7da447d7b94190c6cc73aad21fc2bc89cb5f60
Chronos8001 <Antoine.rudnianyn@ynov.com>
2026-03-25 16:57:51 +0100

    Add files via upload

---
+++ aaaammjjhhss/index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+let pokemons = require('./db-pokemons');
+let helper = require('./helper');
+
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.get('/', (req, res) => {
+    res.send(`<h3>Hello, YBoosST TEAM !</h3>`);
+});
+
+
+app.get('/api/pokemons', (req, res) => {
+    const message = `List of ${pokemons.length} * pokemons`;
+    res.json( helper.success(message, pokemons) );    
+});
+
+app.get('/api/pokemons/:id/:name', (req, res) => {
+    const id = req.params.id;
+    const name = req.params.name;
+    res.send(`<h3>About Pokemon ${id} : ${name} !</h3>`);
+});
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`<h3>About Pokemon ${id} !</h3>`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
